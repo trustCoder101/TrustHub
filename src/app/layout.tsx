ded3e3220338437ecaf3f8b9fc7d27af4b4fa336
trustCoder101 <136416848+trustCoder101@users.noreply.github.com>
2024-02-08 19:10:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Sero AI Trusthub",
+  description: "Sero AI Trusthub",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.variable}>
+        <UserProvider>
+          <div className="min-h-screen overflow-x-hidden grid">{children}</div>
+        </UserProvider>
+      </body>
+    </html>
+  );
+}
